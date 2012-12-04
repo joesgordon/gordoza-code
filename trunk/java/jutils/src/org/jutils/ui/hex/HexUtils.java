9b7f66c9fc1718bcc2d5c5ffef6b5a884c9e6688
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2012-12-04 16:27:26 +0000

    Added functions to HexUtils.

--- trunk/java/jutils/src/org/jutils/ui/hex/HexUtils.java
+++ trunk/java/jutils/src/org/jutils/ui/hex/HexUtils.java
@@ -1,5 +1,11 @@
 package org.jutils.ui.hex;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.PatternSyntaxException;
+
+import org.jutils.NumberParsingUtils;
+
 /*******************************************************************************
  * 
  ******************************************************************************/
@@ -15,6 +21,13 @@ public class HexUtils
         {
             BYTE_STRINGS[i] = toHexString( i );
         }
+    }
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    private HexUtils()
+    {
     }
 
     public static int toUnsigned( byte b )
@@ -37,5 +50,55 @@ public class HexUtils
         }
 
         return s;
+    }
+
+    public static String toHexString( List<Byte> sync )
+    {
+        return toHexString( sync, "" );
+    }
+
+    public static String toHexString( List<Byte> sync, String delim )
+    {
+        StringBuilder str = new StringBuilder();
+
+        for( int i = 0; i < sync.size(); i++ )
+        {
+            if( i > 0 )
+            {
+                str.append( delim );
+            }
+
+            str.append( toHexString( sync.get( i ) & 0xFF ) );
+        }
+
+        return str.toString();
+    }
+
+    public static String trimHexString( String messyString )
+        throws PatternSyntaxException
+    {
+        return messyString.replaceAll( "[^0-9a-fA-F]", "" );
+    }
+
+    public static List<Byte> fromHexString( String text )
+        throws NumberFormatException
+    {
+        if( text.length() % 2 != 0 )
+        {
+            throw new NumberFormatException(
+                "The string must be an even number of hexadecimal digits." );
+        }
+
+        int len = text.length() / 2;
+        List<Byte> bytes = new ArrayList<Byte>( len );
+        String byteStr;
+
+        for( int i = 0; i < len; i++ )
+        {
+            byteStr = text.substring( i * 2, i * 2 + 2 );
+            bytes.add( ( byte )NumberParsingUtils.parseHexByte( byteStr ) );
+        }
+
+        return bytes;
     }
 }
