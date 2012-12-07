f89896c220e3bcd8d38d2cc3bdfb362c191b074c
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2012-12-07 19:35:47 +0000

    Added javadoc.

--- trunk/java/jutils/src/org/jutils/io/IReader.java
+++ trunk/java/jutils/src/org/jutils/io/IReader.java
@@ -3,13 +3,18 @@ package org.jutils.io;
 import java.io.IOException;
 
 /*******************************************************************************
- * @param <T>
+ * Defines a generic method of reading a type from a resource.
+ * @param <T> the type to be read.
+ * @param <R> the resource from which the type is read.
  ******************************************************************************/
 public interface IReader<T, R>
 {
     /***************************************************************************
-     * @return
-     * @throws IOException
+     * Reads the type from the provided resource. Implementing classes should
+     * augment the throws clause to also throw a {@link RuntimeException} for
+     * invalid data formats or other non-I/O errors that might arise.
+     * @return the type read from the provided resource.
+     * @throws IOException any I/O error that occurs.
      **************************************************************************/
     public T read( R resource ) throws IOException;
 }
