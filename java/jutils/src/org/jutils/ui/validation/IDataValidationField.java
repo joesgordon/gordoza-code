e89a65c3baec42b05489217f84642bdce714c1b7
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2013-02-26 18:02:16 +0000

    Added new interface.

---
+++ java/jutils/src/org/jutils/ui/validation/IDataValidationField.java
@@ -0,0 +1,10 @@
+package org.jutils.ui.validation;
+
+import org.jutils.ui.model.IDataView;
+
+/*******************************************************************************
+ * @param <T>
+ ******************************************************************************/
+public interface IDataValidationField<T> extends IDataView<T>, IValidationField
+{
+}
