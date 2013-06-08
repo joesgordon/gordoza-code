ca76e8327c34398a0bed498efa793336d4b4b148
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2013-06-08 03:19:13 +0000

    Updated validation code. Added/removed constructors. Moved IntegerValidator. Added HexBytesValidator.

--- java/jutils/src/org/jutils/ui/validators/HexIntegerValidator.java
+++ java/jutils/src/org/jutils/ui/validators/HexIntegerValidator.java
@@ -1,6 +1,7 @@
 package org.jutils.ui.validators;
 
 import org.jutils.NumberParsingUtils;
+import org.jutils.ui.hex.HexUtils;
 import org.jutils.ui.validation.ValidationException;
 
 /*******************************************************************************
@@ -9,6 +10,22 @@ import org.jutils.ui.validation.ValidationException;
  ******************************************************************************/
 public class HexIntegerValidator implements IDataValidator<Integer>
 {
+    /** The minimum bound, inclusive; not checked if {@code null}. */
+    private final Integer min;
+    /** The maximum bound, inclusive; not checked if {@code null}. */
+    private final Integer max;
+
+    public HexIntegerValidator()
+    {
+        this( null, null );
+    }
+
+    public HexIntegerValidator( Integer min, Integer max )
+    {
+        this.min = min;
+        this.max = max;
+    }
+
     /***************************************************************************
      * 
      **************************************************************************/
@@ -17,7 +34,23 @@ public class HexIntegerValidator implements IDataValidator<Integer>
     {
         try
         {
-            return NumberParsingUtils.parseHexInteger( text );
+            int i = NumberParsingUtils.parseHexInteger( text );
+
+            if( min != null && i < min )
+            {
+                throw new ValidationException( "Value less than minimum: " +
+                    HexUtils.toHexString( i ) + " < " +
+                    HexUtils.toHexString( min ) );
+            }
+
+            if( max != null && i > max )
+            {
+                throw new ValidationException( "Value greater than maximum: " +
+                    HexUtils.toHexString( i ) + " > " +
+                    HexUtils.toHexString( max ) );
+            }
+
+            return i;
         }
         catch( NumberFormatException ex )
         {
