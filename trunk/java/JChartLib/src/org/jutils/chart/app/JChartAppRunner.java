ebe637256acbefdefb2f154563e80570accf7807
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2013-10-04 12:29:51 +0000

    Did completely unimportant stuff.

--- trunk/java/JChartLib/src/org/jutils/chart/app/JChartAppRunner.java
+++ trunk/java/JChartLib/src/org/jutils/chart/app/JChartAppRunner.java
@@ -20,7 +20,7 @@ public class JChartAppRunner extends FrameRunner
 
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
-        frame.setSize( 500, 500 );
+        frame.setSize( 700, 700 );
 
         return frame;
     }
