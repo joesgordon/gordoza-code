d2f329e928a66c9f4e5a9d182abde89e1adb3eea
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-01-14 20:38:17 +0000

    Added comments to MaxQueue.

--- java/jutils/src/org/jutils/utils/MaxQueue.java
+++ java/jutils/src/org/jutils/utils/MaxQueue.java
@@ -3,62 +3,101 @@ package org.jutils.utils;
 import java.util.ArrayDeque;
 import java.util.Iterator;
 
+/*******************************************************************************
+ * Defines a queue that contain zero to the provided maximum number of unique
+ * elements.
+ * @param <T> The type of element in this queue.
+ ******************************************************************************/
 public class MaxQueue<T> implements Iterable<T>
 {
-    /**  */
+    /** The collection elements in this queue. */
     private final ArrayDeque<T> elements;
-    /**  */
+    /** The maximum number of elements in this queue. */
     private final int maxCount;
 
-    /**
-     * @param maxElementCount
-     */
+    /***************************************************************************
+     * Creates a new queue to contain up to the provided number of unique
+     * elements.
+     * @param maxElementCount the maximum number of unique elements in this
+     * queue.
+     **************************************************************************/
     public MaxQueue( int maxElementCount )
     {
         this.maxCount = maxElementCount;
         this.elements = new ArrayDeque<T>( maxElementCount );
     }
 
+    /***************************************************************************
+     * 
+     **************************************************************************/
     @Override
     public Iterator<T> iterator()
     {
         return elements.iterator();
     }
 
+    /***************************************************************************
+     * Returns the number of elements in this queue.
+     * @return the number of elements in this queue.
+     **************************************************************************/
     public int size()
     {
         return elements.size();
     }
 
+    /***************************************************************************
+     * Returns {@code true} if this queue contains no elements.
+     * @return {@code true} if this queue contains no elements.
+     **************************************************************************/
     public boolean isEmpty()
     {
         return elements.isEmpty();
     }
 
+    /***************************************************************************
+     * Removes a single instance of the specified element from this queue. If
+     * the queue does not contain the element, it is unchanged. More formally,
+     * removes the first element {@code e} such that {@code o.equals(e)} (if
+     * such an element exists). Returns {@code true} if this queue contained the
+     * specified element (or equivalently, if this queue changed as a result of
+     * the call).
+     * @param o the element to be removed from this queue, if present.
+     * @return {@code true} if this queue contained the specified element
+     **************************************************************************/
     public boolean remove( Object o )
     {
         return elements.remove( o );
     }
 
+    /***************************************************************************
+     * Removes all of the elements from this queue. The queue will be empty
+     * after this call returns.
+     **************************************************************************/
     public void clear()
     {
         elements.clear();
     }
 
+    /***************************************************************************
+     * Adds the provided element to the beginning of the queue only if it is not
+     * already added.
+     * @param e the element to be added.
+     * @return {@code true}, if the queue was modified, {@code false} otherwise.
+     **************************************************************************/
     public boolean push( T e )
     {
-        boolean changed = elements.add( e );
+        boolean changed = false;
 
-        if( size() > maxCount )
+        if( !elements.contains( e ) )
         {
-            elements.remove();
+            changed = elements.add( e );
+
+            if( size() > maxCount )
+            {
+                elements.remove();
+            }
         }
 
         return changed;
-    }
-
-    public T first()
-    {
-        return elements.getFirst();
     }
 }
