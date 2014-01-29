c4f63ab4c5f572667021e4496d1b0261bd89dbb9
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-01-29 05:46:02 +0000

    Added getOrder function.

--- trunk/java/jutils/src/org/jutils/io/IDataStream.java
+++ trunk/java/jutils/src/org/jutils/io/IDataStream.java
@@ -1,9 +1,14 @@
 package org.jutils.io;
 
 import java.io.IOException;
+import java.nio.ByteOrder;
+
+//TODO comments
 
 public interface IDataStream extends IStream
 {
+    public ByteOrder getOrder();
+
     public boolean readBoolean() throws IOException;
 
     public short readShort() throws IOException;
