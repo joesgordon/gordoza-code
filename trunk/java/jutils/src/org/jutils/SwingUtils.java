e18d933847be52ffe05bd6c0f35b7028d5dd7f62
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-02-02 04:09:27 +0000

    Added ability to install ESC to close JFrames.

--- trunk/java/jutils/src/org/jutils/SwingUtils.java
+++ trunk/java/jutils/src/org/jutils/SwingUtils.java
@@ -1,5 +1,6 @@
 package org.jutils;
 
+import java.awt.Window;
 import java.awt.event.*;
 
 import javax.swing.*;
@@ -12,23 +13,41 @@ public final class SwingUtils
     {
     }
 
-    public static void installEscapeCloseOperation( final JDialog dialog )
+    public static void installEscapeCloseOperation( JFrame frame )
     {
-        Action dispatchClosing = new AbstractAction()
-        {
-            public void actionPerformed( ActionEvent event )
-            {
-                dialog.dispatchEvent( new WindowEvent( dialog,
-                    WindowEvent.WINDOW_CLOSING ) );
-            }
-        };
+        installEscapeCloseOperation( frame, frame.getRootPane() );
+    }
 
+    public static void installEscapeCloseOperation( JDialog dialog )
+    {
+        installEscapeCloseOperation( dialog, dialog.getRootPane() );
+    }
+
+    private static void installEscapeCloseOperation( Window win,
+        JRootPane rootPane )
+    {
+        CloseAction dispatchClosing = new CloseAction( win );
         String mapKey = "com.spodding.tackline.dispatch:WINDOW_CLOSING";
         KeyStroke escapeStroke = KeyStroke.getKeyStroke( KeyEvent.VK_ESCAPE, 0 );
-        JRootPane root = dialog.getRootPane();
 
-        root.getInputMap( JComponent.WHEN_IN_FOCUSED_WINDOW ).put(
+        rootPane.getInputMap( JComponent.WHEN_IN_FOCUSED_WINDOW ).put(
             escapeStroke, mapKey );
-        root.getActionMap().put( mapKey, dispatchClosing );
+        rootPane.getActionMap().put( mapKey, dispatchClosing );
+    }
+
+    private static class CloseAction extends AbstractAction
+    {
+        private final Window win;
+
+        public CloseAction( Window win )
+        {
+            this.win = win;
+        }
+
+        @Override
+        public void actionPerformed( ActionEvent event )
+        {
+            win.dispatchEvent( new WindowEvent( win, WindowEvent.WINDOW_CLOSING ) );
+        }
     }
 }
