1177479cea0350a5096420a3bf0288dc33667d26
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-02-05 15:11:44 +0000

    Added ItemCloner, StreamInput, StreamOutput. Fixed bug in ByteArrayStream. Made BufferedReadOnlyStream more efficient.

---
+++ trunk/java/jutils/src/org/jutils/io/ItemCloner.java
@@ -0,0 +1,40 @@
+package org.jutils.io;
+
+import java.io.IOException;
+
+//TODO comments
+
+public class ItemCloner<T>
+{
+    private final ByteArrayStream stream;
+    private final StreamOutput outputStream;
+    private final StreamInput inputStream;
+
+    public ItemCloner()
+    {
+        this.stream = new ByteArrayStream( 1024 );
+        this.outputStream = new StreamOutput( stream );
+        this.inputStream = new StreamInput( stream );
+    }
+
+    public T cloneItem( T item ) throws IllegalStateException
+    {
+        T clone = null;
+
+        try
+        {
+            stream.seek( 0 );
+            XStreamUtils.writeObjectXStream( item, outputStream );
+
+            stream.seek( 0 );
+            clone = XStreamUtils.readObjectXStream( inputStream );
+        }
+        catch( IOException ex )
+        {
+            throw new IllegalStateException( "Could not clone item: " +
+                ex.getMessage(), ex );
+        }
+
+        return clone;
+    }
+}
---
+++ trunk/java/jutils/src/org/jutils/io/StreamInput.java
@@ -0,0 +1,34 @@
+package org.jutils.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+//TODO comments
+
+public class StreamInput extends InputStream
+{
+    private final IStream stream;
+
+    public StreamInput( IStream stream )
+    {
+        this.stream = stream;
+    }
+
+    @Override
+    public int read() throws IOException
+    {
+        return stream.read() & 0xFF;
+    }
+
+    @Override
+    public int read( byte b[], int off, int len ) throws IOException
+    {
+        return stream.read( b, off, len );
+    }
+
+    @Override
+    public int available() throws IOException
+    {
+        return ( int )stream.getAvailable();
+    }
+}
