d4658e28f1192a9086a7420b30e1903d550c737b
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-02-22 20:30:57 +0000

    Added constructor and comment blocks to DoubleFormField.

--- trunk/java/jutils/src/org/jutils/ui/validation/DoubleFormField.java
+++ trunk/java/jutils/src/org/jutils/ui/validation/DoubleFormField.java
@@ -5,14 +5,37 @@ import java.awt.Component;
 import org.jutils.ui.StandardFormView.IFormField;
 import org.jutils.ui.validators.*;
 
+/*******************************************************************************
+ * Defines an {@link IFormField} that contains a double validater.
+ ******************************************************************************/
 public class DoubleFormField implements IFormField
 {
+    /**  */
     private final String name;
+    /**  */
     private final ValidationTextView textField;
+    /**  */
     private final IUpdater<Double> updater;
 
+    /**  */
     private double value;
 
+    /***************************************************************************
+     * @param name
+     * @param units
+     * @param columns
+     **************************************************************************/
+    public DoubleFormField( String name, String units, int columns )
+    {
+        this( name, units, columns, null );
+    }
+
+    /***************************************************************************
+     * @param name
+     * @param units
+     * @param columns
+     * @param updater
+     **************************************************************************/
     public DoubleFormField( String name, String units, int columns,
         IUpdater<Double> updater )
     {
@@ -27,29 +50,44 @@ public class DoubleFormField implements IFormField
         textField.getField().setValidator( textValidator );
     }
 
+    /***************************************************************************
+     * 
+     **************************************************************************/
     @Override
     public String getFieldName()
     {
         return name;
     }
 
+    /***************************************************************************
+     * 
+     **************************************************************************/
     @Override
     public Component getField()
     {
         return textField.getView();
     }
 
+    /***************************************************************************
+     * @return
+     **************************************************************************/
     public double getValue()
     {
         return value;
     }
 
+    /***************************************************************************
+     * @param value
+     **************************************************************************/
     public void setValue( double value )
     {
         this.value = value;
         textField.setText( "" + value );
     }
 
+    /***************************************************************************
+     * @param editable
+     **************************************************************************/
     public void setEditable( boolean editable )
     {
         textField.getField().getView().setEditable( false );
@@ -71,7 +109,10 @@ public class DoubleFormField implements IFormField
         public void update( Double data )
         {
             view.value = data;
-            view.updater.update( data );
+            if( view.updater != null )
+            {
+                view.updater.update( data );
+            }
         }
     }
 }
