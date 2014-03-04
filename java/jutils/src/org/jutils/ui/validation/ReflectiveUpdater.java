4978f5f0d5581f4c5ee5dd2a15e2ea4ca99a6a01
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-03-04 13:55:12 +0000

    Added ReflectiveUpdater and StandardFormView.addField( String, Component ).

---
+++ java/jutils/src/org/jutils/ui/validation/ReflectiveUpdater.java
@@ -0,0 +1,64 @@
+package org.jutils.ui.validation;
+
+import java.lang.reflect.Field;
+
+/*******************************************************************************
+ * @param <T>
+ ******************************************************************************/
+public class ReflectiveUpdater<T> implements IUpdater<T>
+{
+    /**  */
+    private final Object obj;
+    /**  */
+    private final String structureName;
+    /**  */
+    private final String dataName;
+
+    /***************************************************************************
+     * @param obj
+     * @param structureName
+     * @param dataName
+     **************************************************************************/
+    public ReflectiveUpdater( Object obj, String structureName, String dataName )
+    {
+        this.obj = obj;
+        this.structureName = structureName;
+        this.dataName = dataName;
+    }
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    @Override
+    public void update( T data )
+    {
+        try
+        {
+            Class<?> objClass = obj.getClass();
+            Field structField = objClass.getDeclaredField( structureName );
+
+            structField.setAccessible( true );
+
+            Object struct = structField.get( obj );
+            Class<?> structClass = struct.getClass();
+            Field dataField = structClass.getField( dataName );
+            dataField.set( struct, data );
+        }
+        catch( NoSuchFieldException ex )
+        {
+            throw new RuntimeException( ex );
+        }
+        catch( SecurityException ex )
+        {
+            throw new RuntimeException( ex );
+        }
+        catch( IllegalArgumentException ex )
+        {
+            throw new RuntimeException( ex );
+        }
+        catch( IllegalAccessException ex )
+        {
+            throw new RuntimeException( ex );
+        }
+    }
+}
