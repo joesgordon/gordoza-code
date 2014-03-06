0103e3eb92f25eac1cbd51f63f5ebef561a9c7a7
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-03-06 20:20:45 +0000

    Fixed duplicate call to FileOutputStream.close.

--- java/jutils/src/org/jutils/io/XStreamUtils.java
+++ java/jutils/src/org/jutils/io/XStreamUtils.java
@@ -27,8 +27,6 @@ public class XStreamUtils
         {
             stream.close();
         }
-
-        stream.close();
     }
 
     /***************************************************************************
