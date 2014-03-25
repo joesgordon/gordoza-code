18ff60acc23f6a17e773637123044ce6094d71d2
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-03-25 19:49:14 +0000

    Updated hex views.

---
+++ java/jutils/src/org/jutils/ui/hex/ByteCellRenderer.java
@@ -0,0 +1,105 @@
+package org.jutils.ui.hex;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+
+import org.jutils.PropConstants;
+
+/*******************************************************************************
+ * 
+ ******************************************************************************/
+public class ByteCellRenderer implements TableCellRenderer
+{
+    /**  */
+    public static final Color ALTERNATING_ROW_COLOR = new Color( 210, 225, 240 );
+
+    /**  */
+    private final DefaultTableCellRenderer renderer;
+    /**  */
+    private final Color nullColor;
+
+    /**  */
+    private Color highlightColor;
+    /**  */
+    private int offset;
+    /**  */
+    private int len;
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    public ByteCellRenderer()
+    {
+        super();
+
+        this.renderer = new DefaultTableCellRenderer();
+        this.nullColor = ( Color )UIManager.get( PropConstants.UI_PANEL_COLOR );
+
+        this.highlightColor = Color.yellow;
+        this.offset = -1;
+        this.len = -1;
+
+        renderer.setHorizontalAlignment( SwingConstants.CENTER );
+    }
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    @Override
+    public Component getTableCellRendererComponent( JTable table, Object value,
+        boolean isSelected, boolean hasFocus, int row, int col )
+    {
+        renderer.getTableCellRendererComponent( table, value, isSelected,
+            hasFocus, row, col );
+
+        int off = row * 16 + col;
+        boolean isHighlighted = len > -1 && off >= offset && off < offset + len;
+        boolean isAltRow = row % 2 == 1;
+
+        if( value == null )
+        {
+            renderer.setBackground( nullColor );
+            renderer.setBorder( null );
+        }
+        else
+        {
+            if( isHighlighted )
+            {
+                renderer.setBackground( highlightColor );
+                renderer.setForeground( Color.black );
+            }
+            else if( !isSelected )
+            {
+                if( isAltRow )
+                {
+                    renderer.setBackground( ALTERNATING_ROW_COLOR );
+                }
+                else
+                {
+                    renderer.setBackground( null );
+                }
+            }
+        }
+
+        return renderer;
+    }
+
+    public void setHightlightColor( Color c )
+    {
+        this.highlightColor = c;
+    }
+
+    public void setHighlightLength( int length )
+    {
+        this.len = length;
+    }
+
+    public void setHighlightOffset( int offset )
+    {
+        this.offset = offset;
+    }
+}
