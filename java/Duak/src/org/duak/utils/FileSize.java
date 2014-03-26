624392cd8fca9b09f68b15bd9867a397d06a52fb
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-03-26 20:43:36 +0000

    Added popup menu to table to open file or location.

--- java/Duak/src/org/duak/utils/FileSize.java
+++ java/Duak/src/org/duak/utils/FileSize.java
@@ -1,6 +1,7 @@
 package org.duak.utils;
 
 import org.duak.model.LongComparer;
+import org.jutils.io.IOUtils;
 
 /*******************************************************************************
  * 
@@ -8,46 +9,17 @@ import org.duak.model.LongComparer;
 public class FileSize implements Comparable<FileSize>
 {
     /**  */
-    private long size;
+    private final long size;
     /**  */
-    private String str;
+    private final String str;
 
     /***************************************************************************
      * @param size
      **************************************************************************/
     public FileSize( long size )
     {
-        int count = 0;
-        double frac = size;
-        String units;
-
-        for( ; frac > 1024; count++ )
-        {
-            frac /= 1024.0;
-        }
-
-        switch( count )
-        {
-            case 0:
-                units = " bytes";
-                break;
-            case 1:
-                units = " KB";
-                break;
-            case 2:
-                units = " MB";
-                break;
-            case 3:
-                units = " GB";
-                break;
-            default:
-                units = " Really?(" + count + ")";
-                break;
-        }
-
-        // str = frac + units;
-        str = String.format( "%.2f%s", frac, units );
         this.size = size;
+        this.str = IOUtils.byteCount( size );
     }
 
     /***************************************************************************
