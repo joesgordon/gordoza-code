949ae4f60666dbb50b439e7adbf6e68726bc76a3
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-03-30 03:54:57 +0000

    Fixed minimum size of thumb in PositionIndicator.

--- trunk/java/jutils/src/org/jutils/ui/PositionIndicator.java
+++ trunk/java/jutils/src/org/jutils/ui/PositionIndicator.java
@@ -65,7 +65,10 @@ public class PositionIndicator extends JComponent
         int w = ( int )( getWidth() * unitLength / ( double )length ) - 2;
         int h = getHeight() - 2;
 
-        // w = Math.max( w, 20 );
+        if( unitLength > 0 )
+        {
+            w = Math.max( w, 5 );
+        }
 
         g2.fillRoundRect( x, y, w, h, 8, 8 );
 
