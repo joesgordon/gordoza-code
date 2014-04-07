8aa20f5ad1b0df96cd81281bf761ab660505fbf5
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-04-07 14:45:32 +0000

    Added better default colors to AppRunner. Updated PropEdit's.

--- trunk/java/Testbed/src/testbed/propEdit/PropEditMain.java
+++ trunk/java/Testbed/src/testbed/propEdit/PropEditMain.java
@@ -1,16 +1,78 @@
 package testbed.propEdit;
 
 import java.awt.Color;
+import java.util.*;
 import java.util.Map.Entry;
 
-import javax.swing.UIDefaults;
-import javax.swing.UIManager;
+import javax.swing.*;
 
 import org.jutils.io.LogUtils;
+import org.jutils.ui.app.AppRunner;
+import org.jutils.ui.app.IApplication;
 
 public class PropEditMain
 {
     public static void main( String[] args )
+    {
+        IApplication app = new IApplication()
+        {
+            @Override
+            public String getLookAndFeelName()
+            {
+                return null;
+            }
+
+            @Override
+            public void createAndShowUi()
+            {
+                findInDefaults();
+                testUIDefaults();
+            }
+        };
+
+        AppRunner appr = new AppRunner( app );
+        SwingUtilities.invokeLater( appr );
+    }
+
+    private static void findInDefaults()
+    {
+        List<String> keys = new ArrayList<String>();
+
+        UIDefaults defaults = UIManager.getDefaults();
+
+        for( Entry<Object, Object> entry : defaults.entrySet() )
+        {
+            Object key = entry.getKey();
+            Object value = defaults.get( key );
+
+            if( value != null &&
+                Color.class.isAssignableFrom( value.getClass() ) )
+            {
+                Color c = ( Color )value;
+                int rgb = c.getRGB() & 0xFFFFFF;
+
+                if( rgb == 0x0A246A )
+                {
+                    keys.add( key.toString() );
+                }
+            }
+        }
+
+        if( !keys.isEmpty() )
+        {
+            System.out.println( "c = new Color( 0x0A315A );" );
+            System.out.println();
+
+            Collections.sort( keys );
+
+            for( String key : keys )
+            {
+                System.out.println( "UIManager.put( \"" + key + "\", c );" );
+            }
+        }
+    }
+
+    private static void testUIDefaults()
     {
         Color stadBg = new Color( 0x808080 );
         UIDefaults defaults = UIManager.getDefaults();
