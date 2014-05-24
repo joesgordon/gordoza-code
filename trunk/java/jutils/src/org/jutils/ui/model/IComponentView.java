ca0e62eb87900e4e0b18df476e9b8dd0a4fa9395
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-05-24 19:41:49 +0000

    Added comments.

--- trunk/java/jutils/src/org/jutils/ui/model/IComponentView.java
+++ trunk/java/jutils/src/org/jutils/ui/model/IComponentView.java
@@ -3,7 +3,7 @@ package org.jutils.ui.model;
 import java.awt.Component;
 
 /*******************************************************************************
- * 
+ * Defines a view that returns the least specific possible object type.
  ******************************************************************************/
 public interface IComponentView extends IView<Component>
 {
