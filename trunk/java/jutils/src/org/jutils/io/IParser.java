e4cc3ec65c9b135c40cf98ccbefcbf60e992c19d
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-09-09 13:04:24 +0000

    Changed IParser to throw a Validation Exception.

--- trunk/java/jutils/src/org/jutils/io/IParser.java
+++ trunk/java/jutils/src/org/jutils/io/IParser.java
@@ -1,6 +1,8 @@
 package org.jutils.io;
 
+import org.jutils.ui.validation.ValidationException;
+
 public interface IParser<T>
 {
-    public T parse( String str ) throws RuntimeFormatException;
+    public T parse( String str ) throws ValidationException;
 }
