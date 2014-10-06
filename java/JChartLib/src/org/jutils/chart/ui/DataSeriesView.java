3dbd0d29d19b3dd525bb4bcd429aed4a08653abd
joesgordon@gmail.com <joesgordon@gmail.com@cae3a5ee-1835-9880-4d9b-7f05d8146f6d>
2014-10-06 18:48:38 +0000

    Added data view classes.

---
+++ java/JChartLib/src/org/jutils/chart/ui/DataSeriesView.java
@@ -0,0 +1,285 @@
+package org.jutils.chart.ui;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.*;
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import org.jutils.IconConstants;
+import org.jutils.SwingUtils;
+import org.jutils.chart.data.XYPoint;
+import org.jutils.chart.io.DataLineReader;
+import org.jutils.chart.model.Series;
+import org.jutils.io.IOUtils;
+import org.jutils.ui.JGoodiesToolBar;
+import org.jutils.ui.event.*;
+import org.jutils.ui.model.IDataView;
+
+/*******************************************************************************
+ * 
+ ******************************************************************************/
+public class DataSeriesView implements IDataView<Series>
+{
+    /**  */
+    private final JPanel view;
+    /**  */
+    private final SeriesTableModel tableModel;
+    /**  */
+    private final DataCellRenderer cellRenderer;
+
+    /**  */
+    private Series series;
+    private File file;
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    public DataSeriesView()
+    {
+        this.tableModel = new SeriesTableModel();
+        this.cellRenderer = new DataCellRenderer();
+
+        this.view = createView();
+
+        this.file = null;
+        this.series = null;
+    }
+
+    /***************************************************************************
+     * @return
+     **************************************************************************/
+    private JPanel createView()
+    {
+        JPanel panel = new JPanel( new BorderLayout() );
+        JTable table = new JTable( tableModel );
+        JScrollPane scrollPane = new JScrollPane( table );
+
+        table.setDefaultRenderer( Double.class, this.cellRenderer );
+
+        panel.add( createToolbar(), BorderLayout.NORTH );
+        panel.add( scrollPane, BorderLayout.CENTER );
+
+        return panel;
+    }
+
+    private JToolBar createToolbar()
+    {
+        JToolBar toolbar = new JGoodiesToolBar();
+        Action action;
+        ActionListener listener;
+
+        listener = new FileChooserListener( view, "Choose File to Save",
+            new SaveListener( this ), true );
+        action = new ActionAdapter( listener, "Save",
+            IconConstants.loader.getIcon( IconConstants.SAVE_AS_16 ) );
+        SwingUtils.addActionToToolbar( toolbar, action );
+
+        return toolbar;
+    }
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    @Override
+    public Series getData()
+    {
+        return series;
+    }
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    @Override
+    public void setData( Series series )
+    {
+        this.series = series;
+
+        cellRenderer.setSeries( series );
+        tableModel.setSeries( series );
+    }
+
+    public void setFile( File file )
+    {
+        this.file = file;
+    }
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    @Override
+    public JPanel getView()
+    {
+        return view;
+    }
+
+    /***************************************************************************
+     * 
+     **************************************************************************/
+    private static class SeriesTableModel extends AbstractTableModel
+    {
+        private Series series;
+
+        public SeriesTableModel()
+        {
+            series = null;
+        }
+
+        @Override
+        public int getRowCount()
+        {
+            return series.data.getCount();
+        }
+
+        @Override
+        public int getColumnCount()
+        {
+            return 2;
+        }
+
+        public Class<?> getColumnClass( int columnIndex )
+        {
+            return Double.class;
+        }
+
+        public String getColumnName( int col )
+        {
+            switch( col )
+            {
+                case 0:
+                    return "X";
+
+                case 1:
+                    return "Y";
+
+                default:
+                    throw new IllegalArgumentException(
+                        "Column is out of bounds: " + col );
+            }
+        }
+
+        @Override
+        public Object getValueAt( int row, int col )
+        {
+            switch( col )
+            {
+                case 0:
+                    return series.data.getX( row );
+
+                case 1:
+                    return series.data.getY( row );
+
+                default:
+                    throw new IllegalArgumentException(
+                        "Column is out of bounds: " + col );
+            }
+        }
+
+        public void setSeries( Series series )
+        {
+            this.series = series;
+        }
+    }
+
+    private static class DataCellRenderer extends DefaultTableCellRenderer
+    {
+        private Series series;
+
+        public void setSeries( Series series )
+        {
+            this.series = series;
+        }
+
+        public Component getTableCellRendererComponent( JTable table,
+            Object value, boolean isSelected, boolean hasFocus, int row, int col )
+        {
+            super.getTableCellRendererComponent( table, value, isSelected,
+                hasFocus, row, col );
+
+            if( !isSelected && series.data.isHidden( row ) )
+            {
+                Color bg = Color.LIGHT_GRAY;
+                setBackground( bg );
+            }
+
+            return this;
+        }
+    }
+
+    private static class SaveListener implements IFileSelectionListener
+    {
+        private DataSeriesView view;
+
+        public SaveListener( DataSeriesView view )
+        {
+            this.view = view;
+        }
+
+        @Override
+        public File getDefaultFile()
+        {
+            File file = view.file;
+
+            if( file != null )
+            {
+                String ext = IOUtils.getFileExtension( file );
+                String name = IOUtils.removeFilenameExtension( file );
+
+                file = new File( file.getParentFile(), name + "_filtered." +
+                    ext );
+            }
+
+            return file;
+        }
+
+        @Override
+        public void filesChosen( File [] files )
+        {
+            File tofile = files[0];
+            File fromFile = view.file;
+
+            if( fromFile == null )
+            {
+                // TODO display error
+                return;
+            }
+
+            try( FileReader fr = new FileReader( fromFile );
+                 BufferedReader reader = new BufferedReader( fr ) )
+            {
+                try( PrintStream stream = new PrintStream( tofile ) )
+                {
+                    DataLineReader lineReader = new DataLineReader();
+                    String line = null;
+                    int idx = 0;
+                    XYPoint point = null;
+
+                    while( ( line = reader.readLine() ) != null )
+                    {
+                        point = lineReader.read( line );
+
+                        if( point != null )
+                        {
+                            if( !view.series.data.isHidden( idx ) )
+                            {
+                                stream.println( line );
+                            }
+
+                            idx++;
+                        }
+                        else
+                        {
+                            stream.println( line );
+                        }
+                    }
+                }
+            }
+            catch( IOException ex )
+            {
+                // TODO display error
+            }
+        }
+    }
+}
--- java/JChartLib/src/org/jutils/chart/ui/DataView.java
+++ java/JChartLib/src/org/jutils/chart/ui/DataView.java
@@ -1,23 +1,56 @@
 package org.jutils.chart.ui;
 
-import javax.swing.JPanel;
+import java.awt.*;
 
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import org.jutils.chart.model.Series;
+import org.jutils.ui.TitleView;
 import org.jutils.ui.model.IView;
 
 public class DataView implements IView<JPanel>
 {
+    /**  */
     private final JPanel view;
+    /**  */
+    private final JTabbedPane seriesTabs;
 
     public DataView()
     {
+        this.seriesTabs = new JTabbedPane();
         this.view = createView();
     }
 
     private JPanel createView()
     {
-        JPanel panel = new JPanel();
+        JPanel view = new JPanel( new GridBagLayout() );
+        GridBagConstraints constraints;
+        TitleView titleView = new TitleView();
 
-        return panel;
+        titleView.setTitle( "Data" );
+        titleView.setComponent( seriesTabs );
+
+        constraints = new GridBagConstraints( 0, 0, 1, 1, 1.0, 1.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets( 4,
+                4, 4, 4 ), 0, 0 );
+        view.add( titleView.getView(), constraints );
+
+        return view;
+    }
+
+    public void addSeries( Series series )
+    {
+        DataSeriesView view = new DataSeriesView();
+
+        view.setData( series );
+
+        seriesTabs.addTab( series.name, view.getView() );
+    }
+
+    public void clearSeries()
+    {
+        seriesTabs.removeAll();
     }
 
     @Override
