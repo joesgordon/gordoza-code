77bba26a07aca6089f457d173a4f24db28bbfa84
Joseph Gordon <joesgordon@gmail.com>
2016-11-19 11:33:24 -0600

    Fixed serialVersionUID warnings.

--- java/jutils/src/org/jutils/ui/explorer/ExplorerTable.java
+++ java/jutils/src/org/jutils/ui/explorer/ExplorerTable.java
@@ -1,5 +1,6 @@
 package org.jutils.ui.explorer;
 
+import java.awt.Component;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.io.File;
@@ -7,39 +8,51 @@ import java.util.List;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableColumnModel;
+import javax.swing.table.*;
+
+import org.jutils.ui.model.IView;
 
 /*******************************************************************************
  *
  ******************************************************************************/
-public class ExplorerTable extends JTable
+public class ExplorerTable implements IView<JTable>
 {
     /**  */
-    private DefaultTableCellRenderer rightRenderer = new DefaultTableCellRenderer();
-
+    private final DefaultTableCellRenderer rightRenderer;
     /**  */
-    private ExplorerTableModel model = null;
+    private final JTable table;
+    /**  */
+    private final ExplorerTableModel model;
 
     /***************************************************************************
      *
      **************************************************************************/
     public ExplorerTable()
     {
-        super( new ExplorerTableModel() );
-        model = ( ExplorerTableModel )this.getModel();
+        this.model = new ExplorerTableModel();
+        this.table = new JTable( model );
+        this.rightRenderer = new DefaultTableCellRenderer();
 
         rightRenderer.setHorizontalAlignment( SwingConstants.RIGHT );
 
-        this.setShowGrid( false );
-        this.setAutoResizeMode( JTable.AUTO_RESIZE_OFF );
-        this.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
+        table.setShowGrid( false );
+        table.setAutoResizeMode( JTable.AUTO_RESIZE_OFF );
+        table.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
         // this.setAutoCreateRowSorter( true );
-        this.addFocusListener( new ExplorerTable_FocusLostAdapter( this ) );
+        table.addFocusListener( new ExplorerTable_FocusLostAdapter( this ) );
 
-        this.getTableHeader().setReorderingAllowed( false );
+        table.getTableHeader().setReorderingAllowed( false );
 
         clearTable();
+    }
+
+    /***************************************************************************
+     *
+     **************************************************************************/
+    @Override
+    public JTable getView()
+    {
+        return table;
     }
 
     /***************************************************************************
@@ -55,7 +68,7 @@ public class ExplorerTable extends JTable
      **************************************************************************/
     public void clearTable()
     {
-        TableColumnModel columnModel = this.getColumnModel();
+        TableColumnModel columnModel = table.getColumnModel();
         columnModel.getColumn( 0 ).setCellRenderer( new FilenameRenderer() );
         columnModel.getColumn( 0 ).setPreferredWidth( 200 );
         columnModel.getColumn( 1 ).setPreferredWidth( 200 );
@@ -90,11 +103,11 @@ public class ExplorerTable extends JTable
     public File getSelectedFile()
     {
         File file = null;
-        int row = this.getSelectedRow();
+        int row = table.getSelectedRow();
 
         if( row > -1 && row < model.getRowCount() )
         {
-            row = convertRowIndexToModel( row );
+            row = table.convertRowIndexToModel( row );
             ExplorerItem item = model.getExplorerItem( row );
             file = item.getFile();
         }
@@ -108,71 +121,79 @@ public class ExplorerTable extends JTable
     public ExplorerItem getSelectedItem()
     {
         ExplorerItem item = null;
-        int row = this.getSelectedRow();
+        int row = table.getSelectedRow();
 
         if( row > -1 && row < model.getRowCount() )
         {
-            row = this.convertRowIndexToModel( row );
+            row = table.convertRowIndexToModel( row );
             item = model.getExplorerItem( row );
         }
 
         return item;
     }
-}
 
-class ExplorerTable_FocusLostAdapter implements FocusListener
-{
-    private ExplorerTable table = null;
-
-    public ExplorerTable_FocusLostAdapter( ExplorerTable table )
+    private static final class ExplorerTable_FocusLostAdapter
+        implements FocusListener
     {
-        this.table = table;
-    }
+        private ExplorerTable table = null;
 
-    @Override
-    public void focusGained( FocusEvent e )
-    {
-        ;
-    }
-
-    @Override
-    public void focusLost( FocusEvent e )
-    {
-        table.getSelectionModel().clearSelection();
-    }
-}
-
-class FilenameRenderer extends DefaultTableCellRenderer
-{
-    /**  */
-    private static final FileSystemView view = FileSystemView.getFileSystemView();
-
-    public FilenameRenderer()
-    {
-        super();
-    }
-
-    @Override
-    public void setValue( Object value )
-    {
-        super.setValue( value );
-
-        if( value instanceof ExplorerItem )
+        public ExplorerTable_FocusLostAdapter( ExplorerTable table )
         {
-            File file = ( ( ExplorerItem )value ).getFile();
-            if( file != null && file.exists() )
+            this.table = table;
+        }
+
+        @Override
+        public void focusGained( FocusEvent e )
+        {
+            ;
+        }
+
+        @Override
+        public void focusLost( FocusEvent e )
+        {
+            table.table.getSelectionModel().clearSelection();
+        }
+    }
+
+    private static final class FilenameRenderer implements TableCellRenderer
+    {
+        /**  */
+        private static final FileSystemView view = FileSystemView.getFileSystemView();
+        /**  */
+        private final DefaultTableCellRenderer renderer;
+
+        public FilenameRenderer()
+        {
+            this.renderer = new DefaultTableCellRenderer();
+        }
+
+        @Override
+        public Component getTableCellRendererComponent( JTable table,
+            Object value, boolean isSelected, boolean hasFocus, int row,
+            int column )
+        {
+            renderer.getTableCellRendererComponent( table, value, isSelected,
+                hasFocus, row, column );
+
+            if( value instanceof ExplorerItem )
             {
-                Icon icon = view.getSystemIcon( file );
-                setIcon( icon );
+                File file = ( ( ExplorerItem )value ).getFile();
+                if( file != null && file.exists() )
+                {
+                    Icon icon = view.getSystemIcon( file );
+                    renderer.setIcon( icon );
+                }
+                else
+                {
+                    renderer.setIcon( null );
+                }
             }
             else
             {
-                setIcon( null );
+                renderer.setIcon( null );
             }
-        }
-        else
-        {
-            setIcon( null );
+
+            return renderer;
         }
     }
 }
